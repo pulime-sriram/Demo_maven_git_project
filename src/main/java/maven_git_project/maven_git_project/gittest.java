38d01c8c6a30d13659c263dfd0a9230f70674677
PULIME Venkata Sriram <pu20696741@wipro.com>
2026-06-23 11:16:35 +0530

    updated gittest file

--- src/main/java/maven_git_project/maven_git_project/gittest.java
+++ src/main/java/maven_git_project/maven_git_project/gittest.java
@@ -3,5 +3,6 @@ package maven_git_project.maven_git_project;
 public class gittest {
 public static void manin(String args[]) {
 	System.out.println("Hello git");
+	System.out.println("Hello git updated");
 }
 }
